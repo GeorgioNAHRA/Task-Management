b0fa442ce65a0acf98779c6c8c85adf739741454
LIOR <giorgionahra@gmail.com>
2024-12-04 11:20:06 +0100

    Add Footer

---
+++ footer.php
@@ -0,0 +1,57 @@
+    <!-- Footer -->
+    <footer class="bg-dark text-white py-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3">
+                    <h5>MNB</h5>
+                    <ul class="list-unstyled">
+                        <li><a href="#" class="text-white text-decoration-none">Accueil</a></li>
+                        <li><a href="#" class="text-white text-decoration-none">À propos</a></li>
+                        <li><a href="contact.html" class="text-white text-decoration-none">Contact</a></li>
+                        <li><a href="#" class="text-white text-decoration-none">Blog</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-3">
+                    <h5>Produit</h5>
+                    <ul class="list-unstyled">
+                        <li><a href="#" class="text-white text-decoration-none">Fonctionnalités</a></li>
+                        <li><a href="#" class="text-white text-decoration-none">Tarifs</a></li>
+                        <li><a href="#" class="text-white text-decoration-none">Intégrations</a></li>
+                        <li><a href="#" class="text-white text-decoration-none">API</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-3">
+                    <h5>Ressources</h5>
+                    <ul class="list-unstyled">
+                        <li><a href="#" class="text-white text-decoration-none">Documentation</a></li>
+                        <li><a href="#" class="text-white text-decoration-none">Guides</a></li>
+                        <li><a href="#" class="text-white text-decoration-none">Support</a></li>
+                        <li><a href="#" class="text-white text-decoration-none">Communauté</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-3">
+                    <h5>Entreprise</h5>
+                    <ul class="list-unstyled">
+                        <li><a href="#" class="text-white text-decoration-none">À propos</a></li>
+                        <li><a href="#" class="text-white text-decoration-none">Carrières</a></li>
+                        <li><a href="#" class="text-white text-decoration-none">Presse</a></li>
+                        <li><a href="#" class="text-white text-decoration-none">Blog</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-3">
+                    <h5>Suivez-nous</h5>
+                    <ul class="list-unstyled d-flex gap-2">
+                        <li><a href="#" class="text-white"><i class="fab fa-facebook-f"></i></a></li>
+                        <li><a href="#" class="text-white"><i class="fab fa-twitter"></i></a></li>
+                        <li><a href="#" class="text-white"><i class="fab fa-linkedin"></i></a></li>
+                        <li><a href="#" class="text-white"><i class="fab fa-instagram"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="row mt-4">
+                <div class="col-md-12 text-center">
+                    <p class="mb-0">&copy; 2024 MNB. Tous droits réservés.</p>
+                </div>
+            </div>
+        </div>
+    </footer>
