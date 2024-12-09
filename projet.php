51e7bff44ead15a3b73216ee1eb3ed4fd6abf2e8
LIOR <giorgionahra@gmail.com>
2024-12-09 10:17:16 +0100

    Fix navbar in Projet

--- projet.php
+++ projet.php
@@ -113,5 +113,15 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             </div>
         </div>
     </section>
+    <script>
+        let sidebar = document.querySelector(".sidebar");
+        let sidebarBtn = document.querySelector(".sidebarBtn");
+        sidebarBtn.onclick = function () {
+            sidebar.classList.toggle("active");
+            if (sidebar.classList.contains("active")) {
+                sidebarBtn.classList.replace("bx-menu", "bx-menu-alt-right");
+            } else sidebarBtn.classList.replace("bx-menu-alt-right", "bx-menu");
+        };
+    </script>
 </body>
 </html>
